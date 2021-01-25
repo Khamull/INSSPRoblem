7c5c08312fd1a633b35dafdf3b17131c2dc9d399
Charles Path <charles.path@outlook.com>
2021-01-25 16:31:52 -0300

    First Commit
    All the code

---
+++ INSS/CalculadorInss.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INSS
+{
+    public class CalculadorInss : ICalculadorInss
+    {
+        private int Year;
+        private decimal Income;
+        private decimal Discount;
+        public decimal _Discount
+        {
+            get { return Discount; }
+            set { Discount = value; }
+        }
+
+
+        public decimal _Income
+        {
+            get { return Income; }
+            set { Income = value; }
+        }
+
+        public int _Year
+        {
+            get { return Year; }
+            set { Year = value; }
+        }
+
+        private decimal _CalcularDesconto()
+        {
+            IEnumerable<decimal> ceiling, rate;
+            GetVariables(out ceiling, out rate);
+            if (rate.FirstOrDefault() == 0)
+                return ceiling.FirstOrDefault();
+
+            return Calculo(rate.FirstOrDefault(), ceiling.FirstOrDefault());
+        }
+
+        private void GetVariables(out IEnumerable<decimal> ceiling, out IEnumerable<decimal> rate)
+        {
+            var taxes = IRateDictionary.LoadDictionary();
+            var taxesoftheyear = taxes._TaxLane.Where(s => s.Key._Year.Equals(Year));
+            ceiling = taxesoftheyear.Select(s => s.Key._Ceiling);
+            rate = taxesoftheyear.Where(s => s.Key._Year.Equals(Year))
+                .Select(x => x.Value.Where(j => (double)_Income >= (double)j._Min && (double)_Income <= (double)j._Max)
+                .Select(y => y._Irate).FirstOrDefault());
+        }
+
+        private decimal Calculo(decimal rate, decimal ceiling)
+        {
+            return this._Income * (rate / 100) >= ceiling ? ceiling : this._Income * (rate / 100); ;
+        }
+
+        /// <summary>
+		/// Deve retornar o deconto do INSS aplicado ao salário, na determinada data.
+		/// </summary>
+		public decimal CalcularDesconto(DateTime data, decimal salario)
+        {
+            this.Year = data.Year;
+            this.Income = salario;
+            return _CalcularDesconto();
+        }
+
+        
+    }
+}
---
+++ INSS/TaxRangeDictionary.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INSS
+{
+    public class  TaxRate
+    {
+        private decimal Min;
+        private decimal Max;
+        private decimal IRate;
+
+        public decimal _Irate
+        {
+            get { return IRate; }
+            set { IRate = value; }
+        }
+
+        public decimal _Max
+        {
+            get { return Max; }
+            set { Max = value; }
+        }
+
+
+        public decimal _Min
+        {
+            get { return Min; }
+            set { Min = value; }
+        }
+    }
+    public class TaxYear
+    {
+        private int Year;
+        private decimal Ceiling;
+
+        public decimal _Ceiling
+        {
+            get { return Ceiling; }
+            set { Ceiling = value; }
+        }
+
+        public int _Year
+        {
+            get { return Year; }
+            set { Year = value; }
+        }
+    }
+
+    public class IRateDictionary
+    {
+        private Dictionary<TaxYear, List<TaxRate>> TaxLane;
+        public Dictionary<TaxYear, List<TaxRate>> _TaxLane
+        {
+            get { return TaxLane; }
+            set { TaxLane = value; }
+        }
+        public IRateDictionary()
+        {
+            _TaxLane = new Dictionary<TaxYear, List<TaxRate>>();    
+        }
+
+        public static IRateDictionary LoadDictionary()
+        {
+            return LoadTaxeRateDataByYear();
+        }
+
+        private static IRateDictionary LoadTaxeRateDataByYear()//Load taxes from DB for instance, or even from a API
+        {
+            var Years = new List<TaxYear>()
+            {
+                // 2011
+                new TaxYear()
+                {
+                    _Year = 2011
+                     ,
+                    _Ceiling = (decimal)405.86
+                },
+                //2012
+                new TaxYear()
+                {
+                     _Year = 2012
+                    ,
+                    _Ceiling = (decimal)500.00
+                }
+            };
+            var Rates = new List<List<TaxRate>>();
+            //2011
+            var rates = new List<TaxRate>()
+            {
+                new TaxRate()
+                {
+                    _Min = (decimal)0
+                    ,_Max = (decimal)1106.90
+                    ,_Irate = (decimal)8
+                },
+                new TaxRate()
+                {
+                    _Min = (decimal)1106.91
+                    ,_Max = (decimal)1844.83
+                    ,_Irate = (decimal)9
+                },
+                new TaxRate()
+                {
+                    _Min = (decimal)1844.84
+                    ,_Max = (decimal)3689.66
+                    ,_Irate = (decimal)11
+                },
+            };
+            //2012
+            var rates2 = new List<TaxRate>()
+            {
+                new TaxRate()
+                {
+                    _Min = (decimal)0
+                    ,_Max = (decimal)1000.00
+                    ,_Irate = (decimal)7
+                },
+                new TaxRate()
+                {
+                    _Min = (decimal)1000.01
+                    ,_Max = (decimal)1500.00
+                    ,_Irate = (decimal)8
+                },
+                new TaxRate()
+                {
+                    _Min = (decimal)1500.01
+                    ,_Max = (decimal)3000.00
+                    ,_Irate = (decimal)9
+                },
+                new TaxRate()
+                {
+                    _Min = (decimal)3000.01
+                    ,_Max = (decimal)4000.00
+                    ,_Irate = (decimal)11
+                },
+            };
+
+            var RatesByYear = new IRateDictionary();
+            Rates.Add(rates);
+            Rates.Add(rates2);
+
+            RatesByYear._TaxLane.Add(Years[0], Rates[0]);
+            RatesByYear._TaxLane.Add(Years[1], Rates[1]);
+
+            return RatesByYear;
+        }
+    }
+}
